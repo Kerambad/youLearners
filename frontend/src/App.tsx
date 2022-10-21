eb1da6cf4b7526b826cdfbe71a469eda85404471
marek <marek-schuetze@mail.de>
2022-10-21 11:50:38 +0200

    Set up basic project and connecting back and frontend with basic RESTapi to transfer Hello, World message.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
+  const [testMessage, setTestMessage] = useState("")
 
+  useEffect(() => {
+    fetchTestMessage();
+  }, [])
+
+  const fetchTestMessage = () => {
+    axios.get("/api/test")
+      .then((response) => response.data)
+      .then((data) => setTestMessage(data))
+      .catch((error) => console.log(error))
+  }
+  return (
+
+    <div className="App">
+      <h1>{testMessage}</h1>
     </div>
   );
 }
