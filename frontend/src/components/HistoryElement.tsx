886962a776ea0ac7efd4f13df0ece6bb7f9dabd0
marek <marek-schuetze@mail.de>
2022-11-02 08:31:00 +0100

    - add update Video functionality in backend - add edit video component in frontend - small bug fixes

--- frontend/src/components/HistoryElement.tsx
+++ frontend/src/components/HistoryElement.tsx
@@ -1,19 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Video } from '../models/Video'
+
 type HistoryElementProps = {
     video: Video
     removeById: (videoId: string) => void
     loadVideo: (videoId: string) => void
+    updateVideo: (newVideo: Video) => void
 }
 
 export default function HistoryElement(props: HistoryElementProps) {
+
+    const [renderEdit, setRenderEdit] = useState(false)
+    const [newTitle, setNewTitle] = useState(props.video.title)
+
+    function editField() {
+
+        function submitHandler(action: React.FormEvent<HTMLFormElement>) {
+
+            action.preventDefault();
+            props.updateVideo({
+                videoId: props.video.videoId,
+                title: newTitle
+            })
+            setRenderEdit(false)
+        }
+
+        if (!renderEdit) return null;
+        return (
+            <>
+                <form onSubmit={(submitHandler)}>
+                    <div className='form-floating my-1' >
+                        <textarea
+                            className={"form-control my-2 w-100"}
+                            id='title'
+                            placeholder="Title"
+                            value={newTitle}
+                            onChange={(action) => setNewTitle(action.target.value)}
+                            rows={1}
+                            style={{ height: '5em' }}
+                            required={true}
+                        />
+                        <label htmlFor='title'>Title</label>
+                    </div>
+                    <button className='btn btn-danger w-50' type='submit'>Update Title</button>
+                    <button className='btn btn-light w-50' onClick={() => setRenderEdit(false)}>Cancel</button>
+                </form>
+            </>
+        )
+    }
+
     return (
         <>
             <div className='row m-1 px-0 py-1 border border-dark'>
                 <div className='col-8 overflow-scroll p-1' style={{ height: '3em' }} >
                     <p onClick={() => props.loadVideo(props.video.videoId)}>{props.video.title}</p>
                 </div>
-                <div className='col-2 py-0 px-0'>
+                <div className='col-2 py-0 px-0' onClick={() => setRenderEdit(true)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="" height="" fill="currentColor" className="bi bi-pencil border border-dark border-2 rounded p-2" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                     </svg>
@@ -24,6 +66,7 @@ export default function HistoryElement(props: HistoryElementProps) {
                     </svg>
                 </div>
             </div>
+            {editField()}
         </>
     )
 }
--- frontend/src/hooks/useManageVideos.ts
+++ frontend/src/hooks/useManageVideos.ts
@@ -4,42 +4,48 @@ import { Video } from '../models/Video';
 
 export default function useManageVideos() {
   const [videos, setVideos] = useState([])
-  const [currentVideo,setCurrentVideo] = useState<Video>({
-    videoId:"",
+  const [currentVideo, setCurrentVideo] = useState<Video>({
+    videoId: "",
     title: ""
   })
 
-    useEffect(() => {
-        fetchAllVideos();
-      }, [])
-    
-      const fetchAllVideos = () => {
-        axios.get("/api/videos")
-          .then((response) => response.data)
-          .then((data) => setVideos(data))
-          .catch((error) => console.log(error))
-      }
+  useEffect(() => {
+    fetchAllVideos();
+  }, [])
 
-      const addNewVideo = (newVideo: Video) => {
-        axios.post("/api/videos", newVideo)
-        .then((response) => response.data)
-        .then((data) => setCurrentVideo(data))
-        .then(() => fetchAllVideos())
-        .catch(() => fetchSingleVideo(newVideo.videoId));
-      }
+  const fetchAllVideos = () => {
+    axios.get("/api/videos")
+      .then((response) => response.data)
+      .then((data) => setVideos(data))
+      .catch((error) => console.log(error))
+  }
 
-      const fetchSingleVideo = (videoId:string) => { 
-        axios.get("/api/videos/" + videoId)
-        .then((responde) => responde.data)
-        .then((data) => setCurrentVideo(data))
-        .catch((error) => console.log(error))
-       }
+  const addNewVideo = (newVideo: Video) => {
+    axios.post("/api/videos", newVideo)
+      .then((response) => response.data)
+      .then((data) => setCurrentVideo(data))
+      .then(() => fetchAllVideos())
+      .catch(() => fetchSingleVideo(newVideo.videoId));
+  }
 
-       const removeVideoById = (videoId:string) => { 
-        axios.delete("/api/videos/" + videoId)
-        .then(() => fetchAllVideos())
-        .catch((error) => console.log(error))
-        }
+  const fetchSingleVideo = (videoId: string) => {
+    axios.get("/api/videos/" + videoId)
+      .then((responde) => responde.data)
+      .then((data) => setCurrentVideo(data))
+      .catch((error) => console.log(error))
+  }
 
-      return{videos, addNewVideo, currentVideo, fetchSingleVideo, removeVideoById}
+  const removeVideoById = (videoId: string) => {
+    axios.delete("/api/videos/" + videoId)
+      .then(() => fetchAllVideos())
+      .catch((error) => console.log(error))
+  }
+
+  const updateVideo = (newVideo: Video) => {
+    axios.put("/api/videos", newVideo)
+      .then(() => fetchAllVideos())
+      .catch((error) => console.log(error))
+  }
+
+  return { videos, addNewVideo, currentVideo, fetchSingleVideo, removeVideoById, updateVideo }
 }
