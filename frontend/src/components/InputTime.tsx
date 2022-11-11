86c71b7a4c2f61b5c8c9d77247833160a770c6a9
marek <marek-schuetze@mail.de>
2022-11-11 11:58:59 +0100

    - Time input Field

--- frontend/src/components/InputTime.tsx
+++ frontend/src/components/InputTime.tsx
@@ -7,79 +7,49 @@ type InputTimeProps = {
 }
 
 export default function InputTime(props: InputTimeProps) {
-    const [inputValue, setInputValue] = useState({
-        days: Math.floor(props.timeInSeconds / 86400),
-        hours: Math.floor((props.timeInSeconds % 86400) / 3600),
-        minutes: Math.floor((props.timeInSeconds % 3600) / 60),
-        seconds: Math.floor(props.timeInSeconds % 60)
-    })
-    
-    const timeInputInitial = {
-        days: Math.floor(props.timeInSeconds / 86400),
-        hours: Math.floor((props.timeInSeconds % 86400) / 3600),
-        minutes: Math.floor((props.timeInSeconds % 3600) / 60),
-        seconds: Math.floor(props.timeInSeconds % 60)
+
+    type inputTime = {
+        days: number,
+        hours: number,
+        minutes: number,
+        seconds: number
     }
-    let timeInput = timeInputInitial
+
+    const [inputValue, setInputValue] = useState<inputTime>({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    })
 
     function handleInputChange(action: React.ChangeEvent<HTMLInputElement>) {
 
-        // setInputValue((old) => ({ ...old, [action.target.name]: action.target.value }))
-        // console.log("Sec: "+inputValue.seconds);
-        // console.log("min: "+inputValue.minutes);
-        // console.log("hour: "+inputValue.hours);
-        // console.log(inputValue);
-        
+        setInputValue((old) => ({ ...old, [action.target.name]: action.target.value }))
 
-        // let timeInput = {
-        //     days: Math.floor(props.timeInSeconds / 86400),
-        //     hours: Math.floor((props.timeInSeconds % 86400) / 3600),
-        //     minutes: Math.floor((props.timeInSeconds % 3600) / 60),
-        //     seconds: Math.floor(props.timeInSeconds % 60)
-        // }
+        // console.log("Testday:" + inputValue.days);
+        // console.log("Testhur:" + inputValue.hours);
+        // console.log("Testmin:" + inputValue.minutes)
+        // console.log("Testsec:" + inputValue.seconds);
 
-        timeInput = (({ ...timeInput, [action.target.name]: action.target.value }))
-        // console.log(timeInput.seconds);
+        if (inputValue.hours > 23) {
+            setInputValue((time) => ({ ...time, hours: 23 }))
+        }
+        if (inputValue.minutes > 59) {
+            setInputValue((time) => ({ ...time, minutes: 59 }))
+        }
+        if (inputValue.seconds > 59) {
+            setInputValue((time) => ({ ...time, seconds: 59 }))
+        }
 
-        console.log("Testday:" + timeInput.days);
-        console.log("Testhur:" + timeInput.hours);
-        console.log("Testmin:" + timeInput.minutes)
-        console.log("Testsec:" + timeInput.seconds);
-    
+        let tempTime = (inputValue.minutes * 60) +
+            (inputValue.hours * 3600) +
+            (inputValue.days * 86400) +
+            (inputValue.seconds);
 
-        // if (timeInput.hours > 23) {
-        //     setTimeInput((time) => ({ ...time, hours: 23 }))
-        // }
-        // if (timeInput.minutes > 59) {
-        //     setTimeInput((time) => ({ ...time, minutes: 59 }))
-        // }
-        // if (timeInput.seconds > 59) {
-        //     setTimeInput((time) => ({ ...time, seconds: 59 }))
-        // }
+        console.log(tempTime);
 
-        let tempTime = (timeInput.minutes * 60) +
-            (timeInput.hours * 3600) +
-            (timeInput.days * 86400) +
-            (timeInput.seconds);
-
-        // console.log(tempTime);
-
-
-        props.setTimeInSeconds((old) => ({
-            ...old, time: tempTime
-        }))
+        props.setTimeInSeconds((old) => ({...old, time: tempTime}))
     }
-
-    // let testtime = 90061
-    // let testday = Math.floor(testtime / 86400)
-    // let testhour = Math.floor((testtime % 86400) / 3600)
-    // let testmin = Math.floor((testtime % 3600) / 60)
-    // let testsec = Math.floor(testtime % 60)
-
-    // console.log("Testday:" + testday);
-    // console.log("Testhur:" + testhour);
-    // console.log("Testmin:" + testmin)
-    // console.log("Testsec:" + testsec);
 
     // return (
     //     <p className='row mx-2'>
@@ -91,10 +61,10 @@ export default function InputTime(props: InputTimeProps) {
     // )
     return (
         <p className='row mx-2'>
-            <input className='col-2 p-2 border-1' type={"number"}  name='days' value={timeInput.days} onChange={(action) => handleInputChange(action)}></input>
-            <input className='col-2' name='hours' type={"number"}  value={timeInput.hours} onChange={(action) => handleInputChange(action)}></input>
-            <input className='col-2' name='minutes' type={"number"} value={timeInput.minutes} onChange={(action) => handleInputChange(action)}></input>
-            <input className='col-2' name='seconds' type={"number"} value={timeInput.seconds} onChange={(action) => handleInputChange(action)}></input>
+            <input className='col-2 p-2 border-1' type={"number"} min={0} name='days' value={inputValue.days} onChange={(action) => handleInputChange(action)}></input>
+            <input className='col-2' name='hours' type={"number"} min={0} max={59} value={inputValue.hours} onChange={(action) => handleInputChange(action)}></input>
+            <input className='col-2' name='minutes' type={"number"} min={0} max={59} value={inputValue.minutes} onChange={(action) => handleInputChange(action)}></input>
+            <input className='col-2' name='seconds' type={"number"} min={0} max={59} value={inputValue.seconds} onChange={(action) => handleInputChange(action)}></input>
         </p>
     )
 
